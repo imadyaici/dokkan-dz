036f33058ce76d58616c25c13dba116aedd99797
Imad Yaici <yaici.imad@gmail.com>
2026-02-22 13:02:08 +0100

    fix: fb pixel multiple versions error

--- frontend/ui/components/FacebookPixel.tsx
+++ frontend/ui/components/FacebookPixel.tsx
@@ -20,15 +20,20 @@ const FacebookPixelContent = () => {
     <>
       <Script
         id="fb-pixel"
-        src="https://connect.facebook.net/en_US/fbevents.js"
         strategy="afterInteractive"
-        onLoad={() => {
-          // Script is loaded, we can initialize it if needed,
-          // though our utility already handles the shim.
-          if ((window as any).fbq) {
-            (window as any).fbq('init', fpixel.FB_PIXEL_ID);
-            (window as any).fbq('track', 'PageView');
-          }
+        dangerouslySetInnerHTML={{
+          __html: `
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '${fpixel.FB_PIXEL_ID}');
+            fbq('track', 'PageView');
+          `,
         }}
       />
       <noscript>
--- frontend/utils/fpixel.ts
+++ frontend/utils/fpixel.ts
@@ -1,24 +1,5 @@
 export const FB_PIXEL_ID = process.env.NEXT_PUBLIC_FACEBOOK_PIXEL_ID;
 
-// Initialize the fbq function immediately
-if (typeof window !== 'undefined') {
-  const fbqFunction = function (...args: any[]) {
-    if ((window as any).fbq.callMethod) {
-      (window as any).fbq.callMethod.apply((window as any).fbq, args);
-    } else {
-      (window as any).fbq.queue.push(args);
-    }
-  };
-
-  if (!(window as any).fbq) {
-    (window as any).fbq = fbqFunction;
-    (window as any).fbq.push = fbqFunction;
-    (window as any).fbq.loaded = true;
-    (window as any).fbq.version = '2.0';
-    (window as any).fbq.queue = [];
-  }
-}
-
 export const pageview = () => {
   if (typeof window !== 'undefined' && (window as any).fbq) {
     (window as any).fbq('track', 'PageView');
