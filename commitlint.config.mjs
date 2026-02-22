2bf55f6552f6fc7f8db3f67d8c47905691c0cf1f
Imad Yaici <yaici.imad@gmail.com>
2026-02-22 01:08:48 +0100

    chore: test lint staged

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -1,22 +1,22 @@
 export default {
-    extends: ['@commitlint/config-conventional'],
-    rules: {
-        // Enforce lowercase for types and scopes
-        'type-case': [2, 'always', 'lower-case'],
-        'scope-case': [2, 'always', 'lower-case'],
-        // Disable strict limitations on the commit description
-        'subject-case': [0, 'always'],
-        'subject-full-stop': [0, 'never'],
-        // Examples of allowed types:
-        // feat: A new feature
-        // fix: A bug fix
-        // build: Changes that affect the build system or external dependencies
-        // chore: Other changes that don't modify src or test files
-        // ci: Changes to our CI configuration files and scripts
-        // docs: Documentation only changes
-        // style: Changes that do not affect the meaning of the code (white-space, formatting, etc)
-        // refactor: A code change that neither fixes a bug nor adds a feature
-        // perf: A code change that improves performance
-        // test: Adding missing tests or correcting existing tests
-    },
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    // Enforce lowercase for types and scopes
+    'type-case': [2, 'always', 'lower-case'],
+    'scope-case': [2, 'always', 'lower-case'],
+    // Disable strict limitations on the commit description
+    'subject-case': [0, 'always'],
+    'subject-full-stop': [0, 'never'],
+    // Examples of allowed types:
+    // feat: A new feature
+    // fix: A bug fix
+    // build: Changes that affect the build system or external dependencies
+    // chore: Other changes that don't modify src or test files
+    // ci: Changes to our CI configuration files and scripts
+    // docs: Documentation only changes
+    // style: Changes that do not affect the meaning of the code (white-space, formatting, etc)
+    // refactor: A code change that neither fixes a bug nor adds a feature
+    // perf: A code change that improves performance
+    // test: Adding missing tests or correcting existing tests
+  },
 };
